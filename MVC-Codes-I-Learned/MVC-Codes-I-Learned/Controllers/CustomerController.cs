198d4de2d2c61610808e336687995734bc664b9a
Ayşe İlhanlı <ayseilhanl67@gmail.com>
2020-10-10 12:55:36 +0300

    Add Process
    The process of  Adding for Category , product and customer  has been done.

--- MVC-Codes-I-Learned/MVC-Codes-I-Learned/Controllers/CustomerController.cs
+++ MVC-Codes-I-Learned/MVC-Codes-I-Learned/Controllers/CustomerController.cs
@@ -16,5 +16,21 @@ namespace MVC_Codes_I_Learned.Controllers
             var values = db.Customers.ToList();
             return View(values);
         }
+
+        //Sayfa yüklendiğinde sadece tasarımı geriye döndürür.
+        [HttpGet]
+        public ActionResult NewCustomer()
+        {
+            return View();
+        }
+
+        //Sayfada işlem yapılmadığı sürece aşağıdaki veri tabanına ekleme işlemini gerçekleştirmez.
+        [HttpPost]
+        public ActionResult NewCustomer(Customers p1)
+        {
+            db.Customers.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
     }
 }
--- MVC-Codes-I-Learned/MVC-Codes-I-Learned/Controllers/ProductController.cs
+++ MVC-Codes-I-Learned/MVC-Codes-I-Learned/Controllers/ProductController.cs
@@ -16,5 +16,30 @@ namespace MVC_Codes_I_Learned.Controllers
             var values = db.Products.ToList();
             return View(values);
         }
+
+        //Sayfa yüklendiğinde sadece tasarımı geriye döndürür.
+        [HttpGet]
+        public ActionResult NewProduct()
+        {
+            List<SelectListItem> value = (from i in db.Categories.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = i.CategoryName,
+                                              Value = i.CategoryID.ToString()
+                                          }).ToList();
+            ViewBag.vlu = value;
+            return View();
+        }
+
+        //Sayfada işlem yapılmadığı sürece aşağıdaki veri tabanına ekleme işlemini gerçekleştirmez.
+        [HttpPost]
+        public ActionResult NewProduct(Products p1)
+        {
+            var ctgry = db.Categories.Where(m => m.CategoryID == p1.CategoryID).FirstOrDefault();
+            p1.Categories = ctgry;
+            db.Products.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Product");
+        }
     }
 }
